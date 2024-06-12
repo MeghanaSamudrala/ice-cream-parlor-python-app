bc6c6c44721427995f6e94f89d8587041f1be225
MeghanaSamudrala <158796567+MeghanaSamudrala@users.noreply.github.com>
2024-06-12 11:09:30 +0530

    Update applicationparlor.py

--- applicationparlor.py
+++ applicationparlor.py
@@ -1,54 +1,38 @@
-import sqlite3
+from flask import Flask, request, jsonify, render_template, redirect, url_for
+from ice_cream_parlor import IceCreamParlor
 
-class IceCreamParlor:
-    def __init__(self, db_name='ice_cream_parlor.db'):
-        self.conn = sqlite3.connect(db_name)
-        self.cursor = self.conn.cursor()
-
-    def add_flavor(self, name, ingredients):
-        self.cursor.execute('INSERT INTO seasonal_flavors (name, ingredients) VALUES (?, ?)', (name, ingredients))
-        self.conn.commit()
-
-    def add_ingredient(self, name, quantity):
-        self.cursor.execute('INSERT INTO ingredients (name, quantity) VALUES (?, ?)', (name, quantity))
-        self.conn.commit()
-
-    def add_suggestion(self, flavor, suggestion, allergens):
-        self.cursor.execute('INSERT INTO customer_suggestions (flavor, suggestion, allergens) VALUES (?, ?, ?)', (flavor, suggestion, allergens))
-        self.conn.commit()
-
-    def add_allergen(self, name):
-        self.cursor.execute('INSERT INTO allergens (name) VALUES (?)', (name,))
-        self.conn.commit()
-
-    def search_flavors(self, keyword):
-        self.cursor.execute('SELECT * FROM seasonal_flavors WHERE name LIKE ?', ('%' + keyword + '%',))
-        return self.cursor.fetchall()
-
-    def add_to_cart(self, flavor_id, quantity):
-        self.cursor.execute('INSERT INTO cart (flavor_id, quantity) VALUES (?, ?)', (flavor_id, quantity))
-        self.conn.commit()
-
-    def view_cart(self):
-        self.cursor.execute('''
-            SELECT seasonal_flavors.name, cart.quantity 
-            FROM cart 
-            JOIN seasonal_flavors ON cart.flavor_id = seasonal_flavors.id
-        ''')
-        return self.cursor.fetchall()
-
-    def close(self):
-        self.conn.close()
-
-# Example Usage
+app = Flask(__name__)
 parlor = IceCreamParlor()
-parlor.add_flavor('Strawberry', 'Strawberries, Sugar, Milk')
-parlor.add_ingredient('Sugar', 100)
-parlor.add_suggestion('Mango', 'More mango chunks', 'None
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
+@app.route('/cart')
+def cart():
+    cart_items = parlor.view_cart()
+    return render_template('cart.html', cart_items=cart_items)
 
+@app.route('/search')
+def search():
+    keyword = request.args.get('keyword', '')
+    results = parlor.search_flavors(keyword)
+    return render_template('search.html', results=results, keyword=keyword)
 
+@app.route('/add_allergen', methods=['GET', 'POST'])
+def add_allergen():
+    if request.method == 'POST':
+        name = request.form['name']
+        parlor.add_allergen(name)
+        return redirect(url_for('index'))
+    return render_template('add_allergen.html')
 
+@app.route('/add_to_cart', methods=['POST'])
+def add_to_cart():
+    flavor_id = request.form['flavor_id']
+    quantity = request.form['quantity']
+    parlor.add_to_cart(flavor_id, quantity)
+    return redirect(url_for('cart'))
 
-
+if __name__ == '__main__':
+    app.run(debug=True)
